9205871b8b0ee96065fdb4329b0f1aff58892d7a
ItsMeMarty <martylsarmiento@gmail.com>
2020-10-27 18:25:25 -0400

    restructure backend flask for docker

--- Code/Backend/app/project/__init__.py
+++ Code/Backend/app/project/__init__.py
@@ -16,7 +16,7 @@ def create_app():
 
 def initialize_extensions(app):
     CORS(app)
-    app.config['MONGO_URI'] = ""
+    app.config['MONGO_URI'] = "mongodb+srv://TestAdmin:admintest@cluster0.toaff.mongodb.net/devDB?ssl=true&ssl_cert_reqs=CERT_NONE"
     mongo.init_app(app)
 
 
