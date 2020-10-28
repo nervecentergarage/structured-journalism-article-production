2b14ca4aaac973b6861f37672434087297df4485
Martin Sarmiento <40593134+ItsMeMarty@users.noreply.github.com>
2020-10-28 02:21:13 -0400

    Update app.py

--- Code/Backend/app/app.py
+++ Code/Backend/app/app.py
@@ -6,5 +6,5 @@ from project import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT))
+    port = int(os.getenv('PORT'))
     app.run(debug=False, port=port, host='0.0.0.0')
