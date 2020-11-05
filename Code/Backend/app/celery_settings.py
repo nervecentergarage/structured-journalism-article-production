2fd19828f0de91134bf6b3636fcba1a3c8756e57
ItsMeMarty <martylsarmiento@gmail.com>
2020-11-05 13:25:33 -0500

    Update celery_settings.py
    Changed Redis on Heroku to accomodate a larger job queue memory

--- Code/Backend/app/celery_settings.py
+++ Code/Backend/app/celery_settings.py
@@ -1,5 +1,5 @@
 import os
 
 task_serializer  = 'json'
-broker_url = os.environ.get('REDISTOGO_URL', 'redis://127.0.0.1:6379/0')
+broker_url = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')
 accept_content  = ['json']
