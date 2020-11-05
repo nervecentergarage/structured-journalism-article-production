6fa42007c7b2231b75f91e1a31d836bf5a673037
ItsMeMarty <martylsarmiento@gmail.com>
2020-11-05 14:46:52 -0500

    Update tasks.py

--- Code/Backend/app/tasks.py
+++ Code/Backend/app/tasks.py
@@ -42,7 +42,7 @@ from pymongo import MongoClient
 
 app = Celery()
 app.config_from_object("celery_settings")
-
+app.control.rate_limit('app.scrape_news', '10/m')
 np.random.seed(2018)
 
 nltk.download('punkt')
