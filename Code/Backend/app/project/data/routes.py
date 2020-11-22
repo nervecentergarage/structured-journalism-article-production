65c95f11bf330506edfc2e549084d41038aa184d
ItsMeMarty <martylsarmiento@gmail.com>
2020-11-22 08:13:04 -0500

    Update routes.py
    Co-Authored-By: Jessica Damasco Ty <57690300+jdamascoty@users.noreply.github.com>

--- Code/Backend/app/project/data/routes.py
+++ Code/Backend/app/project/data/routes.py
@@ -111,8 +111,13 @@ def get_articles_by_topic(topic_ids):
     for topic in topic_ids:
         topic_dict = {}
         print("Getting snippets with highest compound for topic", topic)
-        snippet_data = list(snippet_collection.find({"topic": int(topic)}).sort("percentage", -1).limit(10))
+        snippet_data_int_topic = list(snippet_collection.find({"topic": int(topic)}).sort("percentage", -1).limit(10))
+        missing_snippets = len(snippet_data_int_topic)
 
+        if missing_snippets != 0:
+            snippet_data_str_topic = list(snippet_collection.find({"topic": topic}).sort("percentage", -1).limit(0))
+
+        snippet_data = snippet_data_int_topic + snippet_data_str_topic
         cleaned_snippet_data = []
 
         if(snippet_data != []):
